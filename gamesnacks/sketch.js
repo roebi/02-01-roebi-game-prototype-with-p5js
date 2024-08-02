d789d1418496e8f25465a048f39053dcc0d19603
Robert Halter <roebi@halter.in>
2024-08-02 16:37:04 +0200

    initial project setup

---
+++ gamesnacks/sketch.js
@@ -0,0 +1,25 @@
+
+// for formatting use https://standardjs.com/
+
+// define variables
+// let colorem = ['background.jpg']
+let oneToOneSquareAspectRatioFrameSize = 1080
+let dim1920 = 1920
+let dim1080 = 1080
+
+// preload assets, i.e. images and sounds
+function preload() {
+  // aimage = loadImage('aimage.jpg')
+  // asound = loadSound('asound.mp3')
+}
+
+// setup the canvas, define fonts, generate initial items, start run loop
+function setup() {
+  createCanvas(oneToOneSquareAspectRatioFrameSize, oneToOneSquareAspectRatioFrameSize);
+  // textFont('A Font')
+}
+
+// draw a new states
+function draw() {
+  background(240)
+}
