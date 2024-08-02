376f43e5d098e2bd9fd0eb05f29c03ecd283887d
Robert Halter <roebi@halter.in>
2024-08-02 16:56:51 +0200

    add some drawing

--- gamesnacks/sketch.js
+++ gamesnacks/sketch.js
@@ -19,7 +19,28 @@ function setup() {
   // textFont('A Font')
 }
 
-// draw a new states
+// draw all new states, runs about 60 times a second
 function draw() {
   background(240)
+  
+  // google color picker: https://g.co/kgs/KsmaESc
+  fill('yellow') // default white
+  stroke('orange') // default black
+  strokeWeight(20)
+  circle(550, 50, 100)
+  // ellipse()
+  // square()
+  // rect()
+  // quad()
+  // triangle()
+  // line()
+  // point()
+  // arc()
+  // textSize(10)
+  // text('text', 80, 100)
+  // text('mouse', mouseX, mouseY)
+  // width: The width of the canvas
+  // height: The height of the canvas
+  // pmouseX: The previous x-coordinate of the mouse
+  // pmouseY:The previous y-coordinate of the mouse
 }
